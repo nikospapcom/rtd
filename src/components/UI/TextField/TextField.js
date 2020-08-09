b592c8d3a796bb69d48550849b3af0ca9a88d246
Nikos Pap <admin@nikospap.com>
2020-08-10 00:59:50 +0300

    TextField component

--- src/components/UI/TextField/TextField.js
+++ src/components/UI/TextField/TextField.js
@@ -4,12 +4,32 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 const TextField = props => {
-  const { type = 'text', name, label, id, error, ...rest } = props;
+  const {
+    type = 'text',
+    name,
+    label,
+    id,
+    valid,
+    validMessage,
+    error,
+    errorMessage,
+    prepend: prependProp,
+    append: appendProp,
+    ...rest
+  } = props;
   const [inputValue, setInputValue] = useState('');
 
   function handleChange(event) {
     setInputValue(event.target.value);
   }
+
+  const prepend = prependProp && (
+    <span className="text-gray-500 ml-1 text-sm">{prependProp}</span>
+  );
+  const append = appendProp && (
+    <span className="text-gray-500 mr-1 text-sm">{appendProp}</span>
+  );
+
   return (
     <div className="mb-4">
       {label && (
@@ -19,24 +39,38 @@ const TextField = props => {
           {label}
         </label>
       )}
-      <input
+      <div
         className={clsx(
-          'appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 leading-tight',
-          error && 'border-red-500'
-        )}
-        id={id}
-        name={name}
-        onChange={handleChange}
-        type={type}
-        value={inputValue}
-        {...rest}
-      />
-      {error && <p className="text-red-500 text-xs mt-2">{error}</p>}
+          'flex items-center border border-gray-300 rounded',
+          error && 'border-red-500',
+          valid && 'border-green-400'
+        )}>
+        {prepend}
+        <input
+          className={clsx(
+            'appearance-none w-full py-2 px-2 text-gray-700 text-sm leading-tight outline-none rounded'
+          )}
+          id={id}
+          name={name}
+          onChange={handleChange}
+          type={type}
+          value={inputValue}
+          {...rest}
+        />
+        {append}
+      </div>
+      {error && <p className="text-red-500 text-xs mt-2">{errorMessage}</p>}
+      {valid && <p className="text-green-400 text-xs mt-2">{validMessage}</p>}
     </div>
   );
 };
 
 TextField.propTypes = {
+  /**
+   * Element placed after the children.
+   */
+  append: PropTypes.node,
+
   /**
    * The className coming from props
    */
@@ -48,9 +82,14 @@ TextField.propTypes = {
   disabled: PropTypes.bool,
 
   /**
-   * The error message
+   * The error bool
    */
-  error: PropTypes.string,
+  error: PropTypes.bool,
+
+  /**
+   * The error message text
+   */
+  errorMessage: PropTypes.string,
 
   /**
    * The input id
@@ -68,9 +107,24 @@ TextField.propTypes = {
   name: PropTypes.string,
 
   /**
+   * Element placed before the children.
+   */
+  prepend: PropTypes.node,
+
+  /**
    * The input type
    */
   type: PropTypes.string,
+
+  /**
+   * The valid bool
+   */
+  valid: PropTypes.bool,
+
+  /**
+   * The valid message text
+   */
+  validMessage: PropTypes.string,
 
   /**
    * The input value
--- src/pages/UI/FormElements/FormElements.js
+++ src/pages/UI/FormElements/FormElements.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { TextField } from '../../../components';
+import PersonIcon from '@material-ui/icons/Person';
 
 const FormElements = () => {
   return (
@@ -11,10 +12,36 @@ const FormElements = () => {
       <div className="inline-block">
         <TextField id="name" label="First Name" name="name" />
         <TextField
-          error="This field is required"
+          error
+          errorMessage="This field is required"
           id="lastname"
           label="Last Name"
           name="lastname"
+        />
+        <TextField
+          valid
+          validMessage="Looks good!"
+          id="lastname"
+          label="Last Name"
+          name="lastname"
+        />
+        <TextField
+          id="username"
+          label="Username"
+          name="username"
+          prepend={<PersonIcon />}
+        />
+        <TextField
+          append="@example.com"
+          id="email"
+          label="Email"
+          name="email"
+        />
+        <TextField
+          id="box"
+          label="Label"
+          name="box"
+          placeholder="This is a placeholder"
         />
       </div>
     </div>
